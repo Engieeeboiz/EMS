e467f0944b80efcbf629d85e7fa10db7b89a82a1
Engieeeboiz <121923626+Engieeeboiz@users.noreply.github.com>
2025-03-11 11:56:05 +0800

    EMS - OJT
    New Repository

---
+++ js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap';
+import './js_ems_employee';
+import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
---
+++ web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ems_auth_controller;
+use App\Http\Controllers\ems_AJAX_controller;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Navigation Routes
+Route::get('/Home', 
+    [ems_AJAX_controller::class, 'homepage_request']
+)->name('ems_homepage');
+
+Route::get('/Employees', 
+    [ems_AJAX_controller::class, 'employee_request']
+)->name('ems_employees');
+
+Route::get('/Expenses',
+[ems_AJAX_controller::class, 'expenses_request']
+)->name('ems_expenses');
+
+Route::get('/Dashboard', 
+[ems_AJAX_controller::class, 'dashboard_request']
+)->name('ems_dashboard');
+
+
+// Authentication Routes
+Route::post('/sign-in',
+    [ems_auth_controller::class, 'sign_in']
+)->name('signin.auth');
+
+Route::middleware([
+    'auth:sanctum', 
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
